8e7ae61274fcae1046159fbd5f4019cf360783ee
Natalie Dimov <112572931+dimovnm@users.noreply.github.com>
2026-01-24 21:41:45 -0600

    theme page rough

---
+++ frontend/src/data/themes/athletes.js
@@ -0,0 +1,33 @@
+const athletes = {
+  id: "athletes",
+  title: "ATHLETES",
+  characters: [
+    {
+      id: "lebronjames",
+      name: "LeBron James",
+      image: "/themes/athletes/lebron.png",
+    },
+    {
+      id: "lionelmessi",
+      name: "Lionel Messi",
+      image: "/themes/athletes/messi.png",
+    },
+    {
+      id: "serenawilliams",
+      name: "Serena Williams",
+      image: "/themes/athletes/serena.png",
+    },
+    {
+      id: "cristianoronaldo",
+      name: "Cristiano Ronaldo",
+      image: "/themes/athletes/ronaldo.png",
+    },
+    {
+      id: "simonebiles",
+      name: "Simone Biles",
+      image: "/themes/athletes/biles.png",
+    },
+  ],
+};
+
+export default athletes;
