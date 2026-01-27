b1ed0c0c84a583d462e7aa717a13d0599058f657
Natalie Dimov <112572931+dimovnm@users.noreply.github.com>
2026-01-27 15:08:49 -0600

    youtubers

--- frontend/src/data/themes/youtubers.js
+++ frontend/src/data/themes/youtubers.js
@@ -3,19 +3,44 @@ const youtubers = {
   title: "YOUTUBERS",
   characters: [
     {
-      id: "mrbeast",
-      name: "MrBeast",
-      image: "/themes/youtubers/mrbeast.png",
+      id: "ajshabeel",
+      name: "AJ Shabeel",
+      image: "/themes/youtubers/AjShabeel.png",
     },
     {
-      id: "markiplier",
-      name: "Markiplier",
-      image: "/themes/youtubers/markiplier.png",
+      id: "alexbotez",
+      name: "Alex Botez",
+      image: "/themes/youtubers/AlexBotez.png",
     },
     {
-      id: "zachking",
-      name: "Zach King",
-      image: "/themes/youtubers/zachking.png",
+      id: "alexmeyers",
+      name: "Alex Meyers",
+      image: "/themes/youtubers/AlexMeyers.png",
+    },
+    {
+      id: "alissaviolet",
+      name: "Alissa Violet",
+      image: "/themes/youtubers/AlissaViolet.png",
+    },
+    {
+      id: "amine",
+      name: "AmineMaTue",
+      image: "/themes/youtubers/Amine.png",
+    },
+    {
+      id: "amixem",
+      name: "Amixem",
+      image: "/themes/youtubers/Amixem.png",
+    },
+    {
+      id: "andreabotez",
+      name: "Andrea Botez",
+      image: "/themes/youtubers/AndreaBotez.png",
+    },
+    {
+      id: "bethanymota",
+      name: "Bethany Mota",
+      image: "/themes/youtubers/BethanyMota.png",
     },
     {
       id: "brentrivera",
@@ -23,9 +48,259 @@ const youtubers = {
       image: "/themes/youtubers/brentrivera.png",
     },
     {
+      id: "bretmanrock",
+      name: "Bretman Rock",
+      image: "/themes/youtubers/Bretman.png",
+    },
+    {
+      id: "cherylporter",
+      name: "Cheryl Porter",
+      image: "/themes/youtubers/CherylPorter.png",
+    },
+    {
+      id: "codyko",
+      name: "Cody Ko",
+      image: "/themes/youtubers/CodyKo.png",
+    },
+    {
+      id: "danandphil",
+      name: "Dan and Phil",
+      image: "/themes/youtubers/DanAndPhil.png",
+    },
+    {
+      id: "daviddobrik",
+      name: "David Dobrik",
+      image: "/themes/youtubers/DavidDobrik.png",
+    },
+    {
       id: "fazerug",
       name: "FaZe Rug",
       image: "/themes/youtubers/fazerug.png",
+    },
+    {
+      id: "glozell",
+      name: "GloZell Green",
+      image: "/themes/youtubers/Glozell.png",
+    },
+    {
+      id: "huda",
+      name: "HudaBeauty",
+      image: "/themes/youtubers/Huda.png",
+    },
+    {
+      id: "jaclynhill",
+      name: "Jaclyn Hill",
+      image: "/themes/youtubers/JaclynHill.png",
+    },
+    {
+      id: "jaidenanimations",
+      name: "JaidenAnimations",
+      image: "/themes/youtubers/JaidenAnimations.png",
+    },
+    {
+      id: "jakewebber",
+      name: "Jake Webber",
+      image: "/themes/youtubers/JakeWebber.png",
+    },
+    {
+      id: "jamescharles",
+      name: "James Charles",
+      image: "/themes/youtubers/JamesCharles.png",
+    },
+    {
+      id: "jeffreestar",
+      name: "Jeffree Star",
+      image: "/themes/youtubers/JefferyStar.png",
+    },
+    {
+      id: "jennamarbles",
+      name: "Jenna Marbles",
+      image: "/themes/youtubers/JennaMarbles.png",
+    },
+    {
+      id: "johnnieguilbert",
+      name: "Johnnie Guilbert",
+      image: "/themes/youtubers/JohnnieGuilbert.png",
+    },
+    {
+      id: "ksi",
+      name: "KSI",
+      image: "/themes/youtubers/KSI.png",
+    },
+    {
+      id: "lexirivera",
+      name: "Lexi Rivera",
+      image: "/themes/youtubers/LexiRivera.png",
+    },
+    {
+      id: "lillysingh",
+      name: "Lilly Singh",
+      image: "/themes/youtubers/LillySingh.png",
+    },
+    {
+      id: "lilypichu",
+      name: "LilyPichu",
+      image: "/themes/youtubers/LilyPichu.png",
+    },
+    {
+      id: "lizakoshy",
+      name: "Liza Koshy",
+      image: "/themes/youtubers/LizaKoshy.png",
+    },
+    {
+      id: "ludwig",
+      name: "Ludwig",
+      image: "/themes/youtubers/Ludwig.png",
+    },
+    {
+      id: "markiplier",
+      name: "Markiplier",
+      image: "/themes/youtubers/markiplier.png",
+    },
+    {
+      id: "markrober",
+      name: "Mark Rober",
+      image: "/themes/youtubers/MarkRober.png",
+    },
+    {
+      id: "marqueesbrown",
+      name: "Marques Brownlee",
+      image: "/themes/youtubers/MarquesBrownLeee.png",
+    },
+    {
+      id: "matpat",
+      name: "MatPat",
+      image: "/themes/youtubers/MatPat.png",
+    },
+    {
+      id: "michaelreeves",
+      name: "Michael Reeves",
+      image: "/themes/youtubers/MichaelReeves.png",
+    },
+    {
+      id: "michellekhare",
+      name: "Michelle Khare",
+      image: "/themes/youtubers/MichelleKhare.png",
+    },
+    {
+      id: "michellephan",
+      name: "Michelle Phan",
+      image: "/themes/youtubers/MichelleP.png",
+    },
+    {
+      id: "moistcr1tikal",
+      name: "MoistCr1tikal",
+      image: "/themes/youtubers/MoistCritikal.png",
+    },
+    {
+      id: "mrbeast",
+      name: "MrBeast",
+      image: "/themes/youtubers/mrbeast.png",
+    },
+    {
+      id: "eva",
+      name: "mylifeaseva",
+      image: "/themes/youtubers/MyLifeAsEva.png",
+    },
+    {
+      id: "nikkietutorials",
+      name: "NikkieTutorials",
+      image: "/themes/youtubers/NikkiTut.png",
+    },
+    {
+      id: "oddonesout",
+      name: "Odd1sOut",
+      image: "/themes/youtubers/Odd1sOut.png",
+    },
+        {
+      id: "patrickstarrr",
+      name: "PatrickStarrr",
+      image: "/themes/youtubers/Starrr.png",
+    },
+    {
+      id: "pewdiepie",
+      name: "PewDiePie",
+      image: "/themes/youtubers/PewDiePie.png",
+    },
+    {
+      id: "raywilliamjohnson",
+      name: "RayWilliamJ",
+      image: "/themes/youtubers/RayWilliamJ.png",
+    },
+    {
+      id: "rclbeauty",
+      name: "RCLBeauty101",
+      image: "/themes/youtubers/RclBeauty.png",
+    },
+    {
+      id: "ricegum",
+      name: "RiceGum",
+      image: "/themes/youtubers/RiceGum.png",
+    },
+    {
+      id: "ryantrahan",
+      name: "Ryan Trahan",
+      image: "/themes/youtubers/Ryan Trahan.png",
+    },
+    {
+      id: "ryanhiga",
+      name: "Ryan Higa",
+      image: "/themes/youtubers/RyanHiga.png",
+    },
+    {
+      id: "samandcolby",
+      name: "Sam and Colby",
+      image: "/themes/youtubers/SamAndColby.png",
+    },
+    {
+      id: "sinjin",
+      name: "Sinjin Drowning",
+      image: "/themes/youtubers/SinjinDrowning.png",
+    },
+    {
+      id: "sketch",
+      name: "Sketch",
+      image: "/themes/youtubers/Sketch.png",
+    },
+    {
+      id: "speed",
+      name: "Speed",
+      image: "/themes/youtubers/Speed.png",
+    },
+    {
+      id: "squeezie",
+      name: "Squeezie",
+      image: "/themes/youtubers/Squeeze.png",
+    },
+    {
+      id: "sssniperwolf",
+      name: "SSSniperWolf",
+      image: "/themes/youtubers/SSSniperwolf.png",
+    },
+    {
+      id: "tati",
+      name: "Tati Westbrook",
+      image: "/themes/youtubers/Tati.png",
+    },
+    {
+      id: "trisha",
+      name: "Trisha Paytas",
+      image: "/themes/youtubers/Trisha.png",
+    },
+    {
+      id: "tyleroakley",
+      name: "Tyler Oakley",
+      image: "/themes/youtubers/Tyler Oakley.png",
+    },
+    {
+      id: "zachking",
+      name: "Zach King",
+      image: "/themes/youtubers/zachking.png",
+    },
+    {
+      id: "zoella",
+      name: "Zoella",
+      image: "/themes/youtubers/Zoella.png",
     },
   ],
 };
