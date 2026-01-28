5e6aab66d377e3619b1cc510d71fce4d53041b44
Natalie Dimov <112572931+dimovnm@users.noreply.github.com>
2026-01-28 12:49:17 -0600

    artisits

--- frontend/src/data/themes/index.js
+++ frontend/src/data/themes/index.js
@@ -1,7 +1,9 @@
 import youtubers from "./youtubers";
 import athletes from "./athletes";
+import artists from "./artists";
 
 export const THEMES = {
   [youtubers.id]: youtubers,
   [athletes.id]: athletes,
+  [artists.id]: artists,
 };
