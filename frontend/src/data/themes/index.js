271c7e77329002bfccc94b71941c8f58dac3f90f
Natalie Dimov <112572931+dimovnm@users.noreply.github.com>
2026-01-29 16:52:49 -0600

    some streamers added

--- frontend/src/data/themes/index.js
+++ frontend/src/data/themes/index.js
@@ -2,10 +2,12 @@ import youtubers from "./youtubers";
 import athletes from "./athletes";
 import artists from "./artists";
 import actors from "./actors";
+import streamers from "./streamers";
 
 export const THEMES = {
   [youtubers.id]: youtubers,
   [athletes.id]: athletes,
   [artists.id]: artists,
   [actors.id]: actors,
+  [streamers.id]: streamers,
 };
